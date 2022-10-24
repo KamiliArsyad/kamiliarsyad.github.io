698831bcd4f58f87539e70415b3fa3931716d54c
KamiliArsyad <arsyad.ik@gmail.com>
2022-10-24 19:13:29 +0800

    added gfm support

--- src/components/TextBox/index.js
+++ src/components/TextBox/index.js
@@ -1,11 +1,12 @@
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 
 export default function TextBox({ text }) {
   return (
     <div>
-      <ReactMarkdown components={ChakraUIRenderer()} children={text} skipHtml />
+      <ReactMarkdown remarkPlugins={[remarkGfm]} components={ChakraUIRenderer()} children={text} skipHtml />
     </div>
   );
 }
