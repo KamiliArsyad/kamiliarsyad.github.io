6d6216c77a128210dbd294e95fc7a6e7e44ddd1e
KamiliArsyad <arsyad.ik@gmail.com>
2023-12-03 17:09:51 +0800

    Display details on CourseReviews

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,52 @@
-import { Container, Divider, Heading, Stack } from "@chakra-ui/react";
+import {
+  Container,
+  Divider,
+  Heading,
+  Stack,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tr,
+} from "@chakra-ui/react";
 import TextBox from "../TextBox";
+
+const isCourseReview = (post) => {
+  return post.courseReview;
+};
+
+const CourseReviewStats = ({ post }) => {
+  return (
+    <TableContainer>
+      <Table>
+        <TableCaption>
+          {post.courseReview.course_title} - {post.courseReview.courseid}{" "}
+        </TableCaption>
+        <Tbody>
+          <Tr>
+            <Td>
+              <b>Course ID</b>
+            </Td>
+            <Td>{post.courseReview.courseid}</Td>
+          </Tr>
+          <Tr>
+            <Td>
+              <b>AY Taken</b>
+            </Td>
+            <Td>{post.courseReview.academic_semester}</Td>
+          </Tr>
+          <Tr>
+            <Td>
+              <b>Taught by</b>
+            </Td>
+            <Td>{post.courseReview.taught_by}</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default function Post({ post }) {
   return (
@@ -8,6 +55,12 @@ export default function Post({ post }) {
       py={6}
     >
       <Stack>
+        {isCourseReview(post) && (
+          <>
+            <CourseReviewStats post={post} />
+            <Divider />{" "}
+          </>
+        )}
         <Heading>{post.title}</Heading>
         <Divider />
         <TextBox text={post.body} />
