026b668056890eeb1ab5027e27b5d5fa92c6a06e
KamiliArsyad <arsyad.ik@gmail.com>
2024-01-28 20:13:19 +0800

    Add SSR

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { hydrate, render } from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import RouteManager from './config/RouteManager';
 import { BrowserRouter } from 'react-router-dom';
 import { theme } from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+const APP = (
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
@@ -17,3 +16,11 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if (root.hasChildNodes()) {
+  hydrate(APP, root);
+} else {
+  render(APP, root);
+}
