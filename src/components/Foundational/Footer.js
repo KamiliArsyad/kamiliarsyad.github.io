86d62ceb883b8cfbcedeea720775e7ef85e0f870
KamiliArsyad <arsyad.ik@gmail.com>
2024-05-26 10:30:52 +0800

    chore: Update footer links in Footer.js

--- src/components/Foundational/Footer.js
+++ src/components/Foundational/Footer.js
@@ -4,19 +4,15 @@ export default function Footer() {
       <footer className="rounded-lg shadow m-4 bg-[#86615c]">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-200 sm:text-center">
-            © 2023 Arsyad Kamili. 
+            © 2023 Arsyad Kamili.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-200 dark:text-gray-100 sm:mt-0">
-            <li>
-              <a href="/" className="mr-4 hover:underline md:mr-6 ">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/" className="hover:underline">
-                Contact
-              </a>
-            </li>
+            <a href="/" className="mr-4 hover:underline md:mr-6 ">
+              About
+            </a>
+            <a href="/" className="hover:underline">
+              Contact
+            </a>
           </ul>
         </div>
       </footer>
